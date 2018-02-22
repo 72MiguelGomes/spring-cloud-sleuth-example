13f26c749baf94662ddebc41e2f39ecd6064ff73
miguelgomes <mgomes@becker.com>
2018-02-22 07:58:01 +0000

    SpringCloudSleuth Initial Commit

--- src/main/java/com/sleuth/example/config/SpringCloudSleuthConfig.java
+++ src/main/java/com/sleuth/example/config/SpringCloudSleuthConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class TraceFilterConfig {
+public class SpringCloudSleuthConfig {
 
     @Bean
     public TraceFilter traceFilter(BeanFactory beanFactory) {
